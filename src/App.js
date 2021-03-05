db450fe6a8c40238daa17ad2e8bff4510f364928
Micheal <mlnguyen@ualberta.ca>
2021-03-05 13:49:41 -0700

    initial api requests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+function getCharacters() {
+  axios.get('https://swapi.dev/api/people/')
+  .then(res => {
+      let characters = res.data
+  });
+}
+
+function getPlanets() {
+  axios.get('https://swapi.dev/api/planets/')
+  .then(res => {
+      let planets = res.data
+  });
+}
+
+function getFilms() {
+  axios.get('https://swapi.dev/api/films/')
+  .then(res => {
+      let films = res.data
+  });
+}
+
 function App() {
 
 

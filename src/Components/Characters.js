f0c327c5a9d4bb9297046622b12e20d8be4f3190
Micheal <mlnguyen@ualberta.ca>
2021-03-06 01:18:01 -0700

    Updated api request for films to be more efficient

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -10,62 +10,72 @@ class Characters extends Component {
         };
       }
 
+    // Updates state with characters and relevant data from swapi
     async componentDidMount(){
         let characters = [];
         await axios.get('https://swapi.dev/api/people/')
             .then(res => {
                 characters = res.data.results;
             });
+
+        let films = [];
+        await axios.get('https://swapi.dev/api/films')
+            .then(res => {
+                films = res.data.results;
+            });
+
         let promises = [];
         characters.forEach(char => {
-            promises.push(axios.get(char.homeworld).then(res => {char.homeworld = res.data.name}))
-            char.filmNames = [];
+            let charFilmNames = [];
             char.films.forEach(film => {
-                promises.push(
-                    axios.get(film).then(res => {
-                        let data = res.data;
-                        let title = 'Episode ' + data.episode_id + ' - ' + data.title;
-                        char.filmNames.push(title);
-                    })
-                )  
-            })
+                let filmDetails = films.find(x => x.url === film);
+                charFilmNames.push(`Episode ${filmDetails.episode_id} - ${filmDetails.title}`);
+            });
+            char.films = charFilmNames.sort();
+            promises.push(axios.get(char.homeworld).then(res => {char.homeworld = res.data.name}));
         });
+
         await Promise.all(promises).then(() => {
             this.setState({characters});
         });
+
     }
     
+    // Capitalizes first letter of a string
+    capitalizeDetail(det){
+        return det.charAt(0).toUpperCase() + det.slice(1);
+    }
 
     render() {
         let characters = this.state.characters;
         return (        
             <CardDeck>
                 {characters.map(char => {
-                return (
-                    <Col md={4} key={char.name}>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>{char.name}</Card.Title>
-                                <b>Height: </b>{char.height} cm<br/>
-                                <b>Mass: </b>{char.mass} kg<br/>
-                                <b>Hair Color: </b>{char.hair_color}<br/>
-                                <b>Eye Color: </b>{char.eye_color}<br/>
-                                <b>Birth Year: </b>{char.birth_year}<br/>
-                                <b>Homeworld: </b>{char.homeworld}<br/><br/>
-                                <b>Appeared In </b>
-                                <ul>
-                                    {char.filmNames.map(film => {
-                                        return (
-                                            <li key={film}>
-                                                {film}
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                )
+                    return (
+                        <Col md={4} key={char.name} className="star-wars-col">
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title><b>{char.name}</b></Card.Title>
+                                    <b>Height: </b>{char.height} cm<br/>
+                                    <b>Mass: </b>{char.mass} kg<br/>
+                                    <b>Hair Color: </b>{char.hair_color === "n/a" ? "N/A" : this.capitalizeDetail(char.hair_color)}<br/>
+                                    <b>Eye Color: </b>{this.capitalizeDetail(char.eye_color)}<br/>
+                                    <b>Birth Year: </b>{this.capitalizeDetail(char.birth_year)}<br/>
+                                    <b>Homeworld: </b>{char.homeworld}<br/><br/>
+                                    <b>Appeared In </b>
+                                    <ul>
+                                        {char.films.map(film => {
+                                            return (
+                                                <li key={film}>
+                                                    {film}
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
                 })}
             </CardDeck>)
     }
